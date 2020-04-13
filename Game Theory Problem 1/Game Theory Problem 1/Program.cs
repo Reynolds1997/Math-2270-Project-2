f47e13b3c52b88f3591ab5b2b567c274ae6c6817
Reynolds1997 <42719867+Reynolds1997@users.noreply.github.com>
2020-04-13 14:44:34 -0600

    Set up program for Problem 1.
    Right now, the program runs N game simulations and outputs the average expected payout, which is what we need for 1a. I'm currently working on making it more user-friendly/easier to apply to problems 1b and 1c. It shouldn't take long.

---
+++ Game Theory Problem 1/Game Theory Problem 1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Game_Theory_Problem_1
+{
+    class Program
+    {
+
+
+
+        public static int[,] gameArray = new int[3, 4] {
+            {-4, 6, -4, 1} ,   /*  initializers for row indexed by 0 */
+            {5, -7, 3, 8} ,   /*  initializers for row indexed by 1 */
+            {-8, 0, 6, -2}   /*  initializers for row indexed by 2 */
+        };
+
+        static void Main(string[] args)
+        {
+            int gamesToRun = 1000;
+            Console.WriteLine("Running " + gamesToRun + " Games");
+
+
+            double averagePayout = runNGames(gamesToRun);
+
+            Console.Write("Average Payout: " + averagePayout);
+            /*
+            int result = runGame(aStrat, bStrat);
+
+            Console.WriteLine("A Strategy: " + aStrat + " B Strategy: " + bStrat);
+            Console.WriteLine("Game result: " + result);
+            */
+        }
+
+
+        public static int runGame(int aStrat, int bStrat)
+        {
+            aStrat--;
+            bStrat--;
+            return gameArray[aStrat, bStrat];
+        }
+
+        public static int randomAStrat()
+        {
+            Random random = new Random();
+            int result = random.Next(1, 3);
+            if(result == 2)
+            {
+                result = 3;
+            }
+            return result;
+        }
+
+        public static int randomBStrat()
+        {
+            Random random = new Random();
+            return random.Next(1, 5);
+        }
+
+        public static double runNGames(int numOfGames)
+        {
+            double averagePayout = 0.0f;
+            for(int i = 0; i < numOfGames; i++)
+            {
+                int aStrat = randomAStrat();
+                int bStrat = randomBStrat();
+
+                averagePayout += runGame(aStrat, bStrat);
+            }
+
+            averagePayout = averagePayout/numOfGames;
+            return averagePayout;
+            
+            
+        }
+
+        
+    }
+
+    
+}
