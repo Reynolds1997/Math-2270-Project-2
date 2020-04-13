17b260950bf6daf869ea34a79b27271dff886d59
Reynolds1997 <42719867+Reynolds1997@users.noreply.github.com>
2020-04-13 15:26:58 -0600

    Added additional methods for problems 1a and 1b

--- Game Theory Problem 1/Game Theory Problem 1/Program.cs
+++ Game Theory Problem 1/Game Theory Problem 1/Program.cs
@@ -6,31 +6,28 @@ namespace Game_Theory_Problem_1
     {
 
 
-
+        //This array stores the possible results shown in the table for the game.
         public static int[,] gameArray = new int[3, 4] {
-            {-4, 6, -4, 1} ,   /*  initializers for row indexed by 0 */
-            {5, -7, 3, 8} ,   /*  initializers for row indexed by 1 */
-            {-8, 0, 6, -2}   /*  initializers for row indexed by 2 */
+            {-4, 6, -4, 1} ,  
+            {5, -7, 3, 8} ,  
+            {-8, 0, 6, -2}   
         };
 
         static void Main(string[] args)
         {
-            int gamesToRun = 1000;
+            int gamesToRun = 100000;
             Console.WriteLine("Running " + gamesToRun + " Games");
 
+            double averagePayout = runNGamesRandStrats(gamesToRun);
 
-            double averagePayout = runNGames(gamesToRun);
+            Console.WriteLine("Average Payout, Random Strategies: " + averagePayout);
 
-            Console.Write("Average Payout: " + averagePayout);
-            /*
-            int result = runGame(aStrat, bStrat);
-
-            Console.WriteLine("A Strategy: " + aStrat + " B Strategy: " + bStrat);
-            Console.WriteLine("Game result: " + result);
-            */
+            runAllAStrats(gamesToRun);
+            runAllBStrats(gamesToRun);
+          
         }
 
-
+      
         public static int runGame(int aStrat, int bStrat)
         {
             aStrat--;
@@ -55,24 +52,77 @@ namespace Game_Theory_Problem_1
             return random.Next(1, 5);
         }
 
-        public static double runNGames(int numOfGames)
+        public static double runNGamesRandStrats(int numOfGames)
         {
             double averagePayout = 0.0f;
             for(int i = 0; i < numOfGames; i++)
             {
                 int aStrat = randomAStrat();
                 int bStrat = randomBStrat();
-
                 averagePayout += runGame(aStrat, bStrat);
             }
 
             averagePayout = averagePayout/numOfGames;
             return averagePayout;
             
-            
         }
 
-        
+
+        public static double runNGamesFixedAStrat(int numOfGames, int aStrategy)
+        {
+            double averagePayout = 0.0f;
+            for (int i = 0; i < numOfGames; i++)
+            {
+                int aStrat = aStrategy;
+                int bStrat = randomBStrat();
+                averagePayout += runGame(aStrat, bStrat);
+            }
+
+            averagePayout = averagePayout / numOfGames;
+            return averagePayout;
+
+        }
+
+        public static double runNGamesFixedBStrat(int numOfGames, int bStrategy)
+        {
+            double averagePayout = 0.0f;
+            for (int i = 0; i < numOfGames; i++)
+            {
+                int aStrat = randomAStrat();
+                int bStrat = bStrategy;
+                averagePayout += runGame(aStrat, bStrat);
+            }
+
+            averagePayout = averagePayout / numOfGames;
+            return averagePayout;
+
+        }
+
+        public static void runAllAStrats(int numGames)
+        {
+            double averagePayout;
+
+            for (int i = 1; i < 4; i++)
+            {
+                averagePayout = runNGamesFixedAStrat(numGames, i);
+                Console.WriteLine("Average Payout, with A Strategy " + i + " and Random B Strategy: " + averagePayout);
+            }
+
+        }
+
+        public static void runAllBStrats(int numGames)
+        {
+            double averagePayout;
+
+            for (int i = 1; i < 5; i++)
+            {
+                averagePayout = runNGamesFixedBStrat(numGames, i);
+                Console.WriteLine("Average Payout, with Random A Strategy and B Strategy " + i + ": " + averagePayout);
+            }
+
+        }
+
+
     }
 
     
