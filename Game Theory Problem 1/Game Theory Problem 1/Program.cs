6de4b70bd499ec29fb14b992f329be2bf49a4ca0
Reynolds1997 <42719867+Reynolds1997@users.noreply.github.com>
2020-04-13 15:41:26 -0600

    Added comments for clarity.

--- Game Theory Problem 1/Game Theory Problem 1/Program.cs
+++ Game Theory Problem 1/Game Theory Problem 1/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* Game Theory Problem 1
+ * A simple program used to simulate game results for game theory problem 1.
+ * 
+ * Evan Parry 
+ * 
+ */
+
+
+using System;
 
 namespace Game_Theory_Problem_1
 {
@@ -15,7 +23,7 @@ namespace Game_Theory_Problem_1
 
         static void Main(string[] args)
         {
-            int gamesToRun = 100000;
+            int gamesToRun = 100000; 
             Console.WriteLine("Running " + gamesToRun + " Games");
 
             double averagePayout = runNGamesRandStrats(gamesToRun);
@@ -28,30 +36,37 @@ namespace Game_Theory_Problem_1
         }
 
       
+        //Runs a game with the given strategies
         public static int runGame(int aStrat, int bStrat)
         {
+            //Makes the given strategies into valid array index coordinates
             aStrat--;
             bStrat--;
+
+            //Grabs the result from the array
             return gameArray[aStrat, bStrat];
         }
 
+        //Generates a random strategy for A following the behavior described in the problem.
         public static int randomAStrat()
         {
             Random random = new Random();
-            int result = random.Next(1, 3);
-            if(result == 2)
+            int result = random.Next(1, 3); //Generates a 1 or a 2
+            if(result == 2) //Turns any 2s into 3s, because A will never randomly pick strategy 2.
             {
                 result = 3;
             }
             return result;
         }
 
+        //Generates a random strategy for B following the behavior described in the problem.
         public static int randomBStrat()
         {
             Random random = new Random();
             return random.Next(1, 5);
         }
 
+        //Runs N games using random strategies for both A and B
         public static double runNGamesRandStrats(int numOfGames)
         {
             double averagePayout = 0.0f;
@@ -68,6 +83,7 @@ namespace Game_Theory_Problem_1
         }
 
 
+        //Runs N games with a fixed strategy for A and a random strategy for B
         public static double runNGamesFixedAStrat(int numOfGames, int aStrategy)
         {
             double averagePayout = 0.0f;
@@ -83,6 +99,7 @@ namespace Game_Theory_Problem_1
 
         }
 
+        //Runs N games with a random strategy for A and a fixed strategy for B
         public static double runNGamesFixedBStrat(int numOfGames, int bStrategy)
         {
             double averagePayout = 0.0f;
@@ -98,6 +115,7 @@ namespace Game_Theory_Problem_1
 
         }
 
+        //Runs all possible A strats with random B strats and outputs the resulting averages to the console
         public static void runAllAStrats(int numGames)
         {
             double averagePayout;
@@ -110,6 +128,7 @@ namespace Game_Theory_Problem_1
 
         }
 
+        //Runs all possible B strats with random A strats and outputs the resulting averages to the console
         public static void runAllBStrats(int numGames)
         {
             double averagePayout;
